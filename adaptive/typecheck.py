e2bc8b6010e979a9c00851d21ee783c8e8e27a55
Abhay Saxena <ark3@email.com>
2015-08-26 13:42:59 -0400

    Add type check helper; make typ or None optional for lists

--- adaptive/typecheck.py
+++ adaptive/typecheck.py
@@ -1,8 +1,29 @@
 # Tools for type checking
 
-def assertListOf(lst, typ):
+def assertListOf(lst, typ, orNone=True):
     assert isinstance(lst, list), lst
-    for idx, value in enumerate(lst):
-        #assert isinstance(value, typ), (idx, value)
-        assert value is None or isinstance(value, typ), (idx, value)
+    if orNone:
+        for idx, value in enumerate(lst):
+            assert value is None or isinstance(value, typ), (idx, value)
+    else:
+        for idx, value in enumerate(lst):
+            assert isinstance(value, typ), (idx, value)
     return True
+
+def emitTypeCheck(out, name, typ, orNone=True):
+    d = dict(name=name, typ=typ.py_name)
+    if typ.name == "void":
+        out("assert %(name)s is None, %(name)s" % d)
+    elif typ.parameters:
+        assert len(typ.parameters) == 1, "Unimplemented: %s" % typ
+        assert typ.name == "List",  "Unimplemented: %s" % typ
+        d["param"] = typ.parameters[0].py_name
+        if orNone:
+            out("assert %(name)s is None or _assertListOf(%(name)s, %(param)s), %(name)s" % d)
+        else:
+            out("_assertListOf(%(name)s, %(param)s), %(name)s" % d)
+    else:
+        if orNone:
+            out("assert %(name)s is None or isinstance(%(name)s, %(typ)s), %(name)s" % d)
+        else:
+            out("assert isinstance(%(name)s, %(typ)s), %(name)s" % d)
